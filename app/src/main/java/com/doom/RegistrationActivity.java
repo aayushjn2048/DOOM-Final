89044c1bea2a780a7af631fc5ae372da85d02f7c
Uzair54 <muzair0510@gmail.com>
2021-02-09 18:30:36 +0400

    Email Verification - Login and Registration Activity

--- app/src/main/java/com/doom/RegistrationActivity.java
+++ app/src/main/java/com/doom/RegistrationActivity.java
@@ -1,14 +1,99 @@
 package com.doom;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.doom.Models.Users;
+import com.doom.databinding.ActivityRegistrationBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    ActivityRegistrationBinding binding;
+    private FirebaseAuth auth;
+    FirebaseDatabase database;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        binding = ActivityRegistrationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        progressDialog = new ProgressDialog(RegistrationActivity.this);
+        progressDialog.setTitle("Creating Account");
+        progressDialog.setMessage("We are creating your account");
+
+        binding.tvreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(RegistrationActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        binding.btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (binding.etEmail.getText().toString().isEmpty()) {
+                    binding.etEmail.setError("This field cannot remain empty");
+                    return;
+                }
+                if (binding.etPassword.getText().toString().isEmpty()) {
+                    binding.etPassword.setError("This field cannot remain empty");
+                    return;
+                }
+                if (binding.etuserName.getText().toString().isEmpty()) {
+                    binding.etuserName.setError("This field cannot remain empty");
+                    return;
+                }
+                progressDialog.show();
+
+                auth.createUserWithEmailAndPassword
+                        (binding.etEmail.getText().toString(), binding.etPassword.getText().toString()).
+                        addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressDialog.dismiss();
+                                if (task.isSuccessful()) {
+                                    final String id = task.getResult().getUser().getUid();
+                                    FirebaseAuth.getInstance().getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Users user = new Users(binding.etuserName.getText().toString(), binding.etEmail.getText().toString(),
+                                                        binding.etPassword.getText().toString());
+
+                                                database.getReference().child("Users").child(id).setValue(user);
+                                                Toast.makeText(RegistrationActivity.this, "Registered Successfully, Please check your email for verification", Toast.LENGTH_LONG).show();
+                                            } else {
+                                                Toast.makeText(RegistrationActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                            }
+
+                                        }
+                                    });
+
+                                } else {
+                                    Toast.makeText(RegistrationActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                }
+
+                            }
+                        });
+
+            }
+        });
+
     }
 }
